9cb664f3f65b5d2375ea40be2d15faeafe34283e
Arom Ruiz <aromruiz633@gmail.com>
2025-09-09 18:47:32 -0500

    1-3

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'mysql.railway.internal',
+  host: 'interchange.proxy.rlwy.net',
+  port: 31412,
   user: 'root',
   password: 'tvomgXVGyKIipWfzvEIlRjqpAzYZgESh',
   database: 'railway',
